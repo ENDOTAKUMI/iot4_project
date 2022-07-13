31fb2256e94e7ea2777186a98777fabb3bd7c6c1
pc001 <en0605taku@gmail.com>
2022-07-13 10:38:40 +0900

    デザインを整えた

--- iot4_project.ino
+++ iot4_project.ino
@@ -28,6 +28,8 @@ byte rateSpot = 0;        // ラップ変数
 long lastBeat = 0;        // 最後の心拍が発生した時刻
 float beatsPerMinute;     // 1分あたりの心拍
 int beatAvg = 0;          // 心拍の平均
+String error = "";        // エラー内容
+String success = "";       // 成功時表示内容
 
 // ◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇◆◇
 // アクセスポイント情報
@@ -94,7 +96,7 @@ void loop() {
 
   if (checkForBeat(irValue) == true) {  //IR値がTrueだったら
     long delta = millis() - lastBeat;   // デルタ = millis() - lastBeat
-                                        // millis() : Arduinoボードがプログラムの実行を開始した時から現在までの時間をミリ秒単位で返します。
+    // millis() : Arduinoボードがプログラムの実行を開始した時から現在までの時間をミリ秒単位で返します。
     lastBeat = millis();                // lastBeat に心拍を測定した時間を代入
 
     beatsPerMinute = 60 / (delta / 1000.0); // 一分間の心拍の計算
@@ -119,8 +121,12 @@ void loop() {
   Serial.print(", Avg BPM=");
   Serial.print(beatAvg);
 
+  error = "";
+  success = "測定中...";
   if (irValue < 50000) { // 指がセンサーに置かれていない場合
     Serial.print("  【エラー】センサーで指を感知できていません");
+    error = "センサーで指を感知できていません";
+    success = "";
   }
 
   Serial.println();
@@ -131,18 +137,48 @@ void handleSample() {
   String html;
 
   //HTML記述
-  html = "<!DOCTYPE html>";
-  html += "<html lang='ja'>";
+
+
+  html = "<code class=\"code-multiline\"><!doctype html>";
+  html += "<html lang=\"ja\">";
   html += "<head>";
   html += "<meta charset=\"utf-8\">";
+  html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">";
   html += "<meta http-equiv=\"refresh\" content=\"2\">";
-  html += "<title>Tech Tok Tech</title>";
+  html += "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">";
+  html += "<title>心拍数測定 | Tech Tok Tech</title>";
   html += "</head>";
   html += "<body>";
-  html += "<h1>Tech Tok Tech!</h1>";
-  html += "<p><h2>beatAvg: " + String(beatAvg) + " </p>";
+  html += "<nav class=\"navbar navbar-light bg-light p-3\">";
+  html += "<div class=\"d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between\">";
+  html += "<a class=\"navbar-brand\" href=\"#\">";
+  html += "心拍数測定";
+  html += "</a>";
+  html += "</div>";
+  html += "<div class=\"col-12 col-md-5 col-lg-8 d-flex align-items-center justify-content-md-end mt-3 mt-md-0\">";
+  html += "<div class=\"mr-3 mt-1\">";
+  html += "<a class=\"button\"  href=\"javascript:location.reload();\" >再度読み込み</a>";
+  html += "</div>";
+  html += "</div>";
+  html += "</nav>";
+  html += "<div class=\"card text-body\" style=\"width: 500px;margin: 0 auto;margin-top: 10px;\">";
+  html += "<h5 class=\"card-header\">心拍数データ</h5>";
+  html += "<div class=\"card-body\">";
+  html += "<h5 class=\"card-title\">データ</h5>";
+  html += "<p class=\"card-text\">" + String(beatAvg) + "BPM</p>";
+  html += "<p class=\"card-text text-success\">" + success + "</p>";
+  html += "<p class=\"card-text text-danger\">" + error + "</p>";
+  html += "</div>";
+  html += "</div>";
+  html += "<script src=\"https://code.jquery.com/jquery-3.5.1.slim.min.js\" integrity=\"sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj\" crossorigin=\"anonymous\"></script>";
+  html += "<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js\" integrity=\"sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN\" crossorigin=\"anonymous\"></script>";
+  html += "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\" integrity=\"sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV\" crossorigin=\"anonymous\"></script>";
   html += "</body>";
   html += "</html>";
+  html += "</code>";
+
+
+
 
   // HTMLを出力する
   server.send(200, "text/html", html);
